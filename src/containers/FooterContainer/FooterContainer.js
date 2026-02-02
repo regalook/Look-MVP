d4f83b0dccf70a35aec6a8adeda61f58bfc2cf05
Gonzalo <rodriguez.terron.gonzalo@gmail.com>
2026-02-02 10:46:14 +0100

    footer translations

--- src/containers/FooterContainer/FooterContainer.js
+++ src/containers/FooterContainer/FooterContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useConfiguration } from '../../context/configurationContext';
 import loadable from '@loadable/component';
+import { useLocale } from '../../context/localeContext';
 
 const SectionBuilder = loadable(
   () => import(/* webpackChunkName: "SectionBuilder" */ '../PageBuilder/PageBuilder'),
@@ -11,6 +12,7 @@ const SectionBuilder = loadable(
 
 const FooterComponent = () => {
   const { footer = {}, topbar } = useConfiguration();
+  const { locale } = useLocale();
 
   // If footer asset is not set, let's not render Footer at all.
   if (Object.keys(footer).length === 0) {
@@ -20,11 +22,20 @@ const FooterComponent = () => {
   // The footer asset does not specify sectionId or sectionType. However, the SectionBuilder
   // expects sectionId and sectionType in order to identify the section. We add those
   // attributes here before passing the asset to SectionBuilder.
+  const normalizedLocale = (locale || '').split('-')[0].toLowerCase();
+  const isSpanish = normalizedLocale === 'es';
+  const blocks = Array.isArray(footer.blocks) ? footer.blocks : [];
+  const isSpanishBlock = b => (b?.blockId || '').toLowerCase().endsWith('-es');
+  const defaultBlocks = blocks.filter(b => !isSpanishBlock(b));
+  const spanishBlocks = blocks.filter(isSpanishBlock);
+  const filteredBlocks = isSpanish ? (spanishBlocks.length > 0 ? spanishBlocks : defaultBlocks) : defaultBlocks;
+
   const footerSection = {
     ...footer,
     sectionId: 'footer',
     sectionType: 'footer',
     linkLogoToExternalSite: topbar?.logoLink,
+    blocks: filteredBlocks,
   };
 
   return <SectionBuilder sections={[footerSection]} />;
